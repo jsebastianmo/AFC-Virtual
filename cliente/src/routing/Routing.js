6d2c456796b3b7c30acdb5b59dc9a997f13fc004
jmosquera <jmosquera@fagorelectronica.com.co>
2024-02-09 08:29:51 -0500

    agregando archivo redirects

--- cliente/src/routing/Routing.js
+++ cliente/src/routing/Routing.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Principal from "../paginas/principal/Principal";
 import AcercaDe from "../paginas/acercaDe/AcercaDe";
+import Pagina404 from "../paginas/404/Pagina404";
 
 const Routing = () => {
     return(
@@ -9,6 +10,7 @@ const Routing = () => {
                 <Route path="/" element = {<AcercaDe />} />
                 <Route path="/principal" element = {<Principal />} />
                 <Route path="/acerca" element = {<AcercaDe />} />
+                <Route path="/*" element = {<Pagina404 />} />
             </Routes>
         </BrowserRouter>
     )
